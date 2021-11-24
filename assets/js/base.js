4935588551f30f66540680cdf5b7ef1fa56bbfe6
juan1993mickey <jcolorado743@gmail.com>
2021-11-23 21:15:30 -0600

    Init branch

---
+++ assets/js/base.js
@@ -0,0 +1,10 @@
+/*
+    Ejercicio 1:
+    Pedir 2 variables por prompt
+    Si a > b dividir entre b
+    Si a < b sumar a más b
+    Si a == B multiplicar ambos numeros
+    imprimir el resultado en consola
+    */
+
+    
