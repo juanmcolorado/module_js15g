fd5ff183e28c2d4899b067c9f2826533712c9448
juan1993mickey <jcolorado743@gmail.com>
2021-11-24 20:09:20 -0600

    alert

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,20 +1,3 @@
-/*
-    Ejercicio 1:
-    Pedir 2 variables por prompt
-    Si a > b dividir entre b
-    Si a < b sumar a más b
-    Si a == B multiplicar ambos numeros
-    imprimir el resultado en consola
-    */
-
-let number1 = parseInt(prompt("ingresa un valor"))
-let number2 = parseInt(prompt("ingresa un segundo valor"))
- 
-if (number1 > number2){
-    console.log("es una division", number1 / number2)
-}else if (number1 < number2){
-    console.log("es una suma", number1 + number2)
-}else (number1 == number2){
-    console.log("es una multiplicacion", number1 * number2)
-}
-/* el else if final si lo quito el if manda error y no corre en el navegador*/
+var gradeCels = parseInt(prompt("Ingresa los grados celsius: "))   
+var gradeFar = (gradeCels*9/5)+32
+    alert (`La conversion es ${gradeFar}°F`)
