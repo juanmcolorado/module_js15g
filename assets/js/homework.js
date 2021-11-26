f848cc65a527c8063dd854f68eb8d67436693588
juan1993mickey <jcolorado743@gmail.com>
2021-11-25 19:21:05 -0600

    Changes for Camarrillo

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -6,39 +6,32 @@
  * es decir:
  * domingo -> 1, lunes -> 2, martes -> 3, etc...
  */
-    var day = prompt("Ingresa un dia de la semana")
-    var dayMod = day.toLowerCase ()
-    switch (dayMod){
-        case "domingo":
-            console.log(dayMod, " -> 1")
-            break
-        
-        case "lunes":
-            console.log(dayMod, " -> 2")
-            break
-        
-        case "martes":
-            console.log(dayMod, " -> 3")
-            break    
-        
-        case "miercoles":
-        case "miércoles":
-            console.log(dayMod, " -> 4")
-            break
-                
-        case "jueves":
-            console.log(dayMod, " -> 5")
-            break
-        
-        case "viernes":
-            console.log(dayMod, " -> 6")
-            break
-
-        case "sábado":
-        case "sabado":
-            console.log(dayMod, " -> 7")
-            break
-        
-        default:
-            console.log("No reconozco el día")
-    }
+    let day = prompt("Ingresa un dia de la semana")
+    let dayMod = day.toLowerCase ()
+        switch (dayMod){
+            case "domingo":
+                console.log(dayMod, " -> 1")
+                break 
+            case "lunes":
+                console.log(dayMod, " -> 2")
+                break
+            case "martes":
+                console.log(dayMod, " -> 3")
+                break    
+            case "miercoles":
+            case "miércoles":
+                console.log(dayMod, " -> 4")
+                break
+            case "jueves":
+                console.log(dayMod, " -> 5")
+                break
+            case "viernes":
+                console.log(dayMod, " -> 6")
+                break
+            case "sábado":
+            case "sabado":
+                console.log(dayMod, " -> 7")
+                break
+            default:
+                console.log("No reconozco el día")
+        }
