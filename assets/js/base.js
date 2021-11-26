a3679ab1f9e93b41531efc26c13392b30b52f5fc
juan1993mickey <jcolorado743@gmail.com>
2021-11-25 18:35:25 -0600

    homework-two

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,58 @@
+/*
 var gradeCels = parseInt(prompt("Ingresa los grados celsius: "))   
 var gradeFar = (gradeCels*9/5)+32
     alert (`La conversion es ${gradeFar}°F`)
+*/
+/*
+
+var a = parseInt(prompt("Ingresa un valor: "))
+var b = parseInt(prompt("Ingresa un segundo valor: "))
+var c = parseInt(prompt("Ingresa un tercer valor: "))
+
+    /*if (!isNan (a) && !isNaN (b) && !isNan (c) ) { 0*/
+        /*
+        if (a === b && a === c){
+            console.log("Los valores son iguales.")
+        }else if (a > b && a > c){
+            console.log("El valor mayor es:", a)
+        }else if (b > a && b > c){
+            console.log("El valor mayor es:", b)
+        }else {
+            console.log("El valor mayor es:", c)
+        }
+    /*    
+    } else {
+     alert("Algunos valores no son númericos")
+    }
+    */
+
+/*
+    var cityTime = prompt("Ingresa el clima de tu ciudad? \n soleado \n lluvioso \n nevando \n nublado")
+    var weater = parseInt(prompt("Ingresa los grados celsius actuales"))
+    var gradefarn = (weater * 9 / 5) + 32
+    
+    if (!isNaN (weater)){
+
+        switch (cityTime){
+            case "soleado":
+                console.log("El clima en tu ciudad esta ", cityTime, "la temperatura es", gradefarn, "°F")
+                break
+            
+            case "lluvioso":
+                console.log("El clima en tu ciudad esta ", cityTime, "la temperatura es", gradefarn, "°F")
+                break
+            
+            case "nevando":
+                console.log("El clima en tu ciudad esta ", cityTime, "la temperatura es", gradefarn, "°F")
+                break    
+            
+            case "nublado":
+                console.log("El clima en tu ciudad esta ", cityTime, "la temperatura es", gradefarn, "°F")
+                break
+
+            default:
+                console.log("No entiendo el clima")
+        }
+    }
+*/
+
--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,20 +1,4 @@
-  /**
- * 
- * Pedir al usuario por prompt un numero entre 1 y 100
- * verificar si es un par o impar
- * imprimir en resultado en consola
- * Máximo de lineas para este ejercicio, 2 lineas de código
- * Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
- */
-
-    var a = parseInt(prompt("Ingresa un número de 1 al 100: "))
-    
-    if(a % 2 == 0){
-        console.log(a,'es numero par.')
-    }else{
-        console.log(a,'es numero impar.')
-    }
- /**
+/*
  * 
  * Pedir al usuario el dia de la semana 
  * p.ej. lunes, martes, ...
@@ -23,51 +7,38 @@
  * domingo -> 1, lunes -> 2, martes -> 3, etc...
  */
     var day = prompt("Ingresa un dia de la semana")
-    switch (day){
-        case "Domingo":
-            console.log("Domingo -> 1")
+    var dayMod = day.toLowerCase ()
+    switch (dayMod){
+        case "domingo":
+            console.log(dayMod, " -> 1")
             break
         
-        case "Lunes":
-            console.log("Lunes -> 2")
+        case "lunes":
+            console.log(dayMod, " -> 2")
             break
         
-        case "Martes":
-            console.log("Martes -> 3")
+        case "martes":
+            console.log(dayMod, " -> 3")
             break    
         
-        case "Miercoles":
-            console.log("Miercoles -> 4")
+        case "miercoles":
+        case "miércoles":
+            console.log(dayMod, " -> 4")
             break
                 
-        case "Jueves":
-            console.log("Jueves -> 5")
+        case "jueves":
+            console.log(dayMod, " -> 5")
             break
         
-        case "Viernes":
-            console.log("Viernes -> 6")
+        case "viernes":
+            console.log(dayMod, " -> 6")
             break
 
-        case "Sábado":
-            console.log("Sábado -> 7")
+        case "sábado":
+        case "sabado":
+            console.log(dayMod, " -> 7")
             break
+        
+        default:
+            console.log("No reconozco el día")
     }
-    /**
- * 
- * Pedir al usuario 2 numeros 
- * Imprimir en consola cual es el menor de los 2
- * Imprimir en consola cual es el mayor de los 2
- */
-
-    var num1 = parseInt(prompt("Ingresa un número: "))
-    var num2 = parseInt(prompt("Ingresa un segundo número diferente al primero: "))
-
-        if (num1 === num2){
-            console.error("los números son iguales")
-        }else if (num1 > num2){
-            console.log(num1, " es mayor a ", num2 )
-            console.log(num2, " es mayor a ", num1 )  
-        }else{
-            console.log(num2, " es mayor a ", num1)
-            console.log(num1, " es mayor a ", num2)
-     }
